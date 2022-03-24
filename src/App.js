448e6f5239bfb1d4b98250701855c1510b19a9fb
Guillermo <guillermozayas2002@gmail.com>
2022-03-23 20:20:30 -0400

    new changes in UI

--- src/App.js
+++ src/App.js
@@ -1,25 +1,45 @@
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import React, { Component } from 'react';
+import './App.css';
+import { CardList } from './components/card-list/card-list.component';
+
+class App extends Component {
+  constructor() {
+    super();
+
+    this.state =
+    {
+      monsters: []
+    };
+  }
+
+  componentDidMount() {
+    fetch('https://jsonplaceholder.typicode.com/users')
+      .then(resp => resp.json())
+      .then(users => this.setState({monsters : users}))
+
+  }
+
+  render() {
+
+    return (
+      <div className="App">
+        <header className="App-header">
+<CardList/>
+          {
+            this.state.monsters.map(m => (
+
+              <h1 key={m.id}>{m.name}</h1>
+
+            ))
+
+          }
+
+        </header>
+      </div>
+    );
+  }
+
 }
 
 export default App;
---
+++ src/components/card-list/card-list.component.jsx
@@ -0,0 +1,6 @@
+import React from "react";
+
+
+export const CardList = (props) =>{
+    return (<div> Hello panita</div>)
+}
