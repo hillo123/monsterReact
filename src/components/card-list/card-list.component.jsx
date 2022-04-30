14a00574ac0bb3d6f25222bc2535edd4cff9d786
Guillermo <guillermozayas2002@gmail.com>
2022-04-29 23:06:57 -0400

    creating a dinamic data

--- src/components/card-list/card-list.component.jsx
+++ src/components/card-list/card-list.component.jsx
@@ -1,6 +1,16 @@
-import React from "react";
+/* eslint-disable */
+import React from 'react';
+import './card-list.styles.css';
+import { Card } from '../card/card.component';
 
 
-export const CardList = (props) =>{
-    return (<div> Hello panita</div>)
-}
+
+export const CardList = props => (
+  <div className='card-list'>
+    {
+      props.monsters.map(
+        (m) => (<Card key={m.id} monster={m} />)
+      )
+    }
+  </div>
+)
