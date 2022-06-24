2a503785c86a87c988f81eb8fc5d21e673b047cd
Guillermo <guillermozayas2002@gmail.com>
2022-06-24 11:45:19 -0400

    adding functionality to search box

--- src/App.js
+++ src/App.js
@@ -20,13 +20,18 @@ class App extends Component {
   }
 
   render() {
+    const { monsters, searchField } = this.state
+    const filteredMonsters = monsters.filter(monster =>
+      monster.name.toLowerCase().includes(searchField.toLowerCase())
+    )
+
     return (
       <div className="App">
         <input type='search' placeholder='search monster'
-        onChange={e=>{
-          this.setState({ searchField: e.target.value }, ()=> { console.log(this.state)}) 
-        }}/>
-        <CardList monsters={this.state.monsters}>
+          onChange={e => {
+            this.setState({ searchField: e.target.value }, () => { console.log(this.state) })
+          }} />
+        <CardList monsters={filteredMonsters}>
         </CardList>
       </div>
     );
