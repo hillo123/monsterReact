932f3dffae715b4b6f39908ba85077e0dc8cb6c9
Guillermo <guillermozayas2002@gmail.com>
2022-09-13 12:12:30 -0400

    component searchbox fixed

--- src/components/search-box/search-box.component.jsx
+++ src/components/search-box/search-box.component.jsx
@@ -1,15 +1,20 @@
 /* eslint-disable linebreak-style */
 /* eslint-disable react/function-component-definition */
 import React from 'react';
+import PropTypes from 'prop-types';
 
 import './search-box.styles.css';
 
 // eslint-disable-next-line import/prefer-default-export
 export const SearchBox = ({ placeholder, handleChange }) => (
   <input
-    className='search'
-    type='search'
+    className="search"
+    type="search"
     placeholder={placeholder}
     onChange={handleChange}
   />
 );
+SearchBox.propTypes = {
+  placeholder: PropTypes.string.isRequired,
+  handleChange: PropTypes.func.isRequired,
+};
